439374595b523c08f41569d0681fe035f26ea8de
Nguyen Ngoc Khanh <khanhjj70@gmail.com>
2022-09-21 17:36:00 +0700

    fix

--- src/TrienLam.js
+++ src/TrienLam.js
@@ -1,20 +1,17 @@
-import banner from "../src/banner.png";
+import banner from "../src/Tieude.png";
 import gttrienlam from "../src/GioithieuTrienlam.png";
-import video from "../src/ĐẠI HỘI ĐOÀN THCS HỒ CHÍ MINH THÀNH PHỐ ĐÀ NẴNG LẦN THỨ XIX, NHIỆM KỲ 2022-2027.mp4"
+import video from "../src/ĐẠI HỘI ĐOÀN THCS HỒ CHÍ MINH THÀNH PHỐ ĐÀ NẴNG LẦN THỨ XIX, NHIỆM KỲ 2022-2027.mp4";
 
 function TriemLam() {
   return (
     <div className=" w-100 trienlam-wrapper d-flex">
       <div className="p-4 d-flex flex-column align-items-center h-100 justify-content-center m-auto">
-        <div className=" text-center mb-3">
-          <img src={banner} className="trienlam-banner" alt="" />
+        <div className=" text-center mb-5 ">
+          <img style={{}} src={banner} className="trienlam-banner" alt="" />
         </div>
         <div className="video-wrapper">
           <video controls loop autoPlay muted>
-            <source
-              src={video}
-              type="video/mp4"
-            />
+            <source src={video} type="video/mp4" />
           </video>
         </div>
         <div className="mt-4 text-center">
